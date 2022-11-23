a373cc1e7c2eb4fc50d247679bcce52028014e14
Uijin-Lee <digipen.uijin.lee@gmail.com>
2022-11-23 02:31:32 -0800

    new json file and read the data in d3

--- D3Framework/jquery/js/main.js
+++ D3Framework/jquery/js/main.js
@@ -5,7 +5,7 @@
     assignment6
  */
 
-var margin = { left:80, right:100, top:50, bottom:100 },
+    var margin = { left:80, right:100, top:50, bottom:100 },
     height = 500 - margin.top - margin.bottom, 
     width = 900 - margin.left - margin.right;
 
@@ -35,76 +35,116 @@ var ySvg = svg.append("g")
     .attr("transform", "translate(50, 50)")
     .call(d3.axisLeft(y));
 
+var Num = $("#numFuncsFromGUI").val();
+
+var Hit = [];
+var arrayNum = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+
 d3.json("data/ProfileReport_DataFix_ver2.json").then(function(data){
-
-    console.log(data);
-
-    data.forEach(function(d){
-        d.Mobile_sub_Average = +d.Mobile_sub_Average;
-        d.GDP_per_capita_Average = +d.GDP_per_capita_Average;
-    });
-/*
-    d3.interval(function(){
-        update(data)
-        flag = !flag
-    }, 1500);*/
-
-    update(data);
+    for(var i = 0; i < arrayNum.length; i++)
+    {
+        var a = 0;
+        data[arrayNum[i]].forEach(function(d){
+            a += +d.HitCount;
+        })
+        Hit.push(a);
+        console.log(Hit[i]);
+    }
+    /*var a = 0;
+    //console.log(data[arrayNum[0]]);
+    data[arrayNum[0]].forEach(function(d){
+        a += +d.HitCount;
+        //console.log(d.HitCount);
+    })
+    Hit.push(a);
+    console.log(Hit[0]);
+    */
+    //console.log(data["one"]);
+    //console.log(data[arrayNum[0]]);
+    //console.log(data[arrayNum[0]].HitCount);
+    //data.Hit_count = +data["one"].Hit_count;
+    /*for(var i = 0; i < arrayNum.length(); i++)
+    {
+        console.log(data[arrayNum[i]])
+        data[arrayNum[i]].map(function(d){
+            var Sum = 0;
+            for(var j = 0; j < Num; j++)
+            {
+                console.log(d[j]);
+                //Sum += (+d[j].Hit_count);
+            }
+            //console.log(Sum);
+        })
+    }*/
+    //console.log(data["one"]);
+    //data[]
+    update();
 });
 
+
+function update(data){
+
     
-/*
-var Perc2Bet = $("#percentToBetFromGUI").val();
-var BlowChance = $("#chanceOfBlowupFromGUI").val();
-var PercBet2Blow = $("#percentOfBetToBlowupFromGUI").val();
-var WinMult = $("#winMultiplierFromGUI").val();
-var TrialNum = $("#numTrialsFromGUI").val();
-var SimsNum = $("#numSimsFromGUI").val();
-var binSize = $("#binNumberFromGUI").val();
-var kneEp = $("#kernelEpFromGUI").val();
-*/
+    /*var value = flag ? "Mobile_sub_Average" : "GDP_per_capita_Average";
 
-//updateSimulation(); //run all simulations and get capital data
+    x.domain(data.map(function(d){ return d.Continent }));
+    y.domain([0, d3.max(data, function(d) { return d[value] })]);
 
-/*
-$("#numTrialsFromGUI").on("change",  function(d){
-    TrialNum = this.value;
-    updateSimulation();
-    updateChart();
-    jitter();
-});
-*/    
-
-/*
-function updateChart()
-{
-    //curve.remove();
-    //xSvg.remove();
-    //ySvg.remove();
-
-    kde = kernelDensityEstimator(kernelEpanechnikov(kneEp), x.ticks(binSize))
-    density = kde(capitalAmount)
-
-    var resizeX = Math.max(...capitalAmount) + 500;
-    var resizeY = Math.max(...density.map(function(x){return x[1];}));
-
-    x.domain([-100, resizeX]);
-
-    xSvg.transition().duration(1000)
-    .call(d3.axisBottom(x));
-
-    y.domain([0, resizeY]);
-
-    ySvg.transition().duration(1000)
-    .call(d3.axisLeft(y));
+    // X-axis
+    var xAxisCall = d3.axisBottom(x);
+    g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0, " + height + ")")
+    .call(xAxisCall)
+    .selectAll("text")
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("font-size", "12px")
     
-    curve
-        .datum(density)
-        .transition()
-        .duration(1000)
-        .attr("d", d3.line().curve(d3.curveBasis)
-            .x(function(d) {return x(d[0]) + 50;})
-            .y(function(d) {return y(d[1]) + 50;})
-        );
+    // Y-axis
+    var yAxisCall = d3.axisLeft(y).tickFormat(function(d){ return d; });
+    yAxisGroup.transition(t).call(yAxisCall);
+        
+    var rects = g.selectAll("rect").data(data, function(d){return d.Continent;});
+
+    rects.exit()
+        .remove();
+
+    rects.enter()
+        .append("rect")
+        .attr("x", function(d){ return x(d.Continent) })
+        .attr("width", x.bandwidth)
+        .attr("y", y(0))
+        .attr("height", 0)
+        .merge(rects)
+        .transition(t)
+        .attr("x", function(d){ return x(d.Continent) })
+        .attr("width", x.bandwidth)
+        .attr("y", function(d){ return y(d[value]); })
+        .attr("height", function(d){ return height - y(d[value]); })
+        .attr("fill", function(d){ 
+            if(d.Continent == "Asia") 
+            {
+                return colors[4];
+            }
+            else if(d.Continent == "Americas")
+            {
+                return colors[0];
+            }
+            else if(d.Continent == "Europe")
+            {
+                return colors[2];
+            }
+            else if(d.Continent == "Oceania")
+            {
+                return colors[1];
+            }
+            else if(d.Continent == "Africa")
+            {
+                return colors[3];
+            }
+        });
+    var label = flag ? "# Mobile Subscriber per 100 people Average " : "GDP Per Capita Average";
+    yLabel.text(label);*/
 }
-*/
+
