f83ca21c4e57a86fd2a49c831ccc8602ab690d2b
elliottHong <tjdgus3174@gmail.com>
2022-11-26 20:30:04 -0800

    Added bar graph

--- D3Framework/jquery/js/main.js
+++ D3Framework/jquery/js/main.js
@@ -4,10 +4,13 @@
     hagyeong.kim
     assignment6
  */
+var Num = 4;
+var Hit = [];
+var arrayNum = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 
-    var margin = { left:80, right:100, top:50, bottom:100 },
-    height = 500 - margin.top - margin.bottom, 
-    width = 900 - margin.left - margin.right;
+var margin = { left:80, right:100, top:50, bottom:100 };
+var height = 500 - margin.top - margin.bottom;
+var width = 900 - margin.left - margin.right;
 
 var svg = d3.select("#chart-area")
     .append("svg")
@@ -23,7 +26,7 @@ var x = d3.scaleLinear()
     .domain([0, 10])
     .range([0, width]);
 
-var xSvg = svg.append("g")
+var xAxisGroup = svg.append("g")
     .attr("transform", "translate(50, " + (height + 50) + ")")
     .call(d3.axisBottom(x));
 
@@ -31,9 +34,15 @@ var y = d3.scaleLinear()
     .range([height, 0])
     .domain([0, 0.01]);
 
-var ySvg = svg.append("g")
+var yAxisGroup = svg.append("g")
     .attr("transform", "translate(50, 50)")
     .call(d3.axisLeft(y));
+
+d3.json("data/ProfileReport_DataFix_ver2.json").then(function(data){
+
+    calculateHitCountByNum(data);
+    updateChart();
+});
 
 var Num = $("#numFuncsFromGUI").val();
 $("#numFuncsFromGUI").on("change",  function(d){
@@ -42,17 +51,8 @@ $("#numFuncsFromGUI").on("change",  function(d){
     d3.json("data/ProfileReport_DataFix_ver2.json").then(function(data){
 
         calculateHitCountByNum(data);
-        update();
+        updateChart();
     });
-});
-
-var Hit = [];
-var arrayNum = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
-
-d3.json("data/ProfileReport_DataFix_ver2.json").then(function(data){
-
-    calculateHitCountByNum(data);l
-    update();
 });
 
 function calculateHitCountByNum(data){
@@ -69,19 +69,36 @@ function calculateHitCountByNum(data){
                 a += +d.HitCount;
                 Limit += 1;
             }
+            d.Hits = +Hit[i];
         })
         Hit.push(a);
         console.log(Hit[i]);
     }
-
 }
 
-function update(){
+
+function updateChart(){
+    var xScale = d3.scaleBand().range([0, width]).padding(0.5);
+    var yScale = d3.scaleLinear().range([height, 0]);
+
     var resizeY = Math.max(...Hit) + 1;
-
     y.domain([0, resizeY]);
+    xScale.domain(Hit);
+    yScale.domain([0, resizeY]);
 
-    ySvg.transition().duration(1000)
+    yAxisGroup.transition().duration(1000)
     .call(d3.axisLeft(y));
+
+    g.selectAll(".bar").remove();
+
+    g.selectAll(".bar")
+    .data(Hit)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return xScale(d); })
+    .attr("y", function(d) { return yScale(d); })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function(d) { return height - yScale(d); });
+
 }
 
